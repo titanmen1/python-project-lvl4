5dd088b0056770ae4b551b8bdca14199737f8bf4
Artem Arkhipov <arkhipov@algomost.ru>
2021-05-12 20:43:26 +0300

    update project 1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 print(db_from_env)
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
