50f5c06a18c0a3973d5778fc116372bfe89e3664
Artem Arkhipov <arkhipov@algomost.ru>
2021-05-12 18:54:57 +0300

    fix bug 12

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 from django.utils import timezone
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(_('name'), max_length=64, unique=True)
 
     def __str__(self):
         return self.name
