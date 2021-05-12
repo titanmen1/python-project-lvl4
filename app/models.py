edd5a10f29855f818a1f91cf3437aafff7d0117c
Artem Arkhipov <arkhipov@algomost.ru>
2021-05-12 21:57:59 +0300

    fix bug 18

--- app/models.py
+++ app/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    def __str__(self):
+        return self.get_full_name()
+
 
 
 class Status(models.Model):
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,6 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
+AUTH_USER_MODEL = 'app.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
